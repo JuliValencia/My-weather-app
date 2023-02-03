1a09f944db2de18f5a2b959c072009951eab6626
JuliValencia <juliana.valencia1@gmail.com>
2023-02-03 15:40:41 +0100

    Added API integration

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 function showTemperature(response) {
-  let temperatureElement = document.querySelector(`#temperature`);
+  let temperatureElement = document.querySelector(`#current-temperature`);
   let cityElement = document.querySelector(`#city`);
   let descriptionElement = document.querySelector(`#description`);
+  let humidityElement = document.querySelector(`#humidity`);
+  let windElement = document.querySelector(`#wind`);
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 let apiKey = `485cb8bac1atfac9f3b46bfdodfc3a40`;
 let city = `Brussels`;
