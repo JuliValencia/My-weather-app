f16985699555eace07ff15bc2618c4bde1c79195
JuliValencia <juliana.valencia1@gmail.com>
2023-02-03 17:30:36 +0100

    Updated time and location

--- src/app.js
+++ src/app.js
@@ -1,11 +1,53 @@
+let now = new Date();
+let currentDateTime = document.querySelector(".current-date-time");
+let date = now.getDate();
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let days = [
+  `Sunday`,
+  `Monday`,
+  `Tuesday`,
+  `Wednesday`,
+  `Thursday`,
+  `Friday`,
+  `Saturday`,
+];
+let day = days[now.getDay()];
+
+let months = [
+  `January`,
+  `February`,
+  `March`,
+  `April`,
+  `May`,
+  `June`,
+  `July`,
+  `August`,
+  `September`,
+  `October`,
+  `November`,
+  `December`,
+];
+let month = months[now.getMonth()];
+currentDateTime.innerHTML = `${day}, ${month} ${date}, ${hours}:${minutes}`;
+
 function showTemperature(response) {
-  let temperatureElement = document.querySelector(`#current-temperature`);
   let cityElement = document.querySelector(`#city`);
+  let countryElement = document.querySelector(`#country`);
+  let temperatureElement = document.querySelector(`#current-temperature`);
   let descriptionElement = document.querySelector(`#description`);
   let humidityElement = document.querySelector(`#humidity`);
   let windElement = document.querySelector(`#wind`);
+
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
+  countryElement.innerHTML = response.data.country;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
